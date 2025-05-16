8fab6bfbe31edf492e246b030d159c8d9e6bf15e
nicholasdzn <nicholasadorni1643@gmail.com>
2025-05-15 21:46:07 -0300

    created initial template

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+const int USER_ID = 1; // Constant for user ID
+
+void insertNewUser() // Function to insert a new user
+{
+    cout << "Insert new user" << endl;
+}
+
+void deleteUser() // Function to delete a user
+{
+    cout << "Delete user" << endl;
+}
+
+void searchUser() // Function to search for a user
+{
+    cout << "Search user" << endl;
+}
+
+void transferMoney() // Function to transfer money
+{
+    cout << "Transfer money" << endl;
+}
+
+void loadFileToMemory() // Function to load a file (.txt) and write it in the memory
+{
+    cout << "Load file" << endl;
+}
+
+int main()
+{
+}
