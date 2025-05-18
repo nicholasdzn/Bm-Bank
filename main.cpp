f64364898619cfb25fd06955c948d3bb622cfa7a
nicholasdzn <nicholasadorni1643@gmail.com>
2025-05-18 10:17:07 -0300

    done function insertNewUser

--- main.cpp
+++ main.cpp
@@ -1,11 +1,61 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
 
-const int USER_ID = 1; // Constant for user ID
+int USER_ID = 1; // Constant for user ID
+
+struct User // Structure to represent a user
+{
+    int id;
+    string name;
+    int age;
+    double balance;
+    User *next;
+};
 
 void insertNewUser() // Function to insert a new user
 {
-    cout << "Insert new user" << endl;
+    static User *head = nullptr; // Head of the linked list
+
+    User *newUser = new User;
+    newUser->id = USER_ID++;
+    cout << "Enter user name: ";
+    cin.ignore();
+    getline(cin, newUser->name);
+    cout << "Enter user age: ";
+    cin >> newUser->age;
+    cout << "Enter initial balance: ";
+    cin >> newUser->balance;
+    newUser->next = nullptr;
+
+    if (head == nullptr)
+    {
+        head = newUser;
+    }
+    else
+    {
+        User *temp = head;
+        while (temp->next != nullptr)
+        {
+            temp = temp->next;
+        }
+        temp->next = newUser;
+    }
+    cout << "User inserted successfully with ID: " << newUser->id << endl;
+
+    // Grava o novo usuário em um arquivo CSV
+    ofstream file("users.txt", ios::app);
+    if (file.is_open())
+    {
+        file << newUser->id << "," << newUser->name << "," << newUser->age << "," << newUser->balance << endl;
+        file.close();
+        cout << "Usuário gravado no arquivo com sucesso." << endl;
+    }
+    else
+    {
+        cout << "Erro ao abrir o arquivo para gravação." << endl;
+    }
 }
 
 void deleteUser() // Function to delete a user
@@ -30,4 +80,42 @@ void loadFileToMemory() // Function to load a file (.txt) and write it in the me
 
 int main()
 {
+    int choice;
+    do
+    {
+        cout << "1. Insert new user" << endl;
+        cout << "2. Delete user" << endl;
+        cout << "3. Search user" << endl;
+        cout << "4. Transfer money" << endl;
+        cout << "5. Load file to memory" << endl;
+        cout << "0. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            insertNewUser();
+            break;
+        case 2:
+            deleteUser();
+            break;
+        case 3:
+            searchUser();
+            break;
+        case 4:
+            transferMoney();
+            break;
+        case 5:
+            loadFileToMemory();
+            break;
+        case 0:
+            cout << "Exiting..." << endl;
+            break;
+        default:
+            cout << "Invalid choice, please try again." << endl;
+        }
+    } while (choice != 0);
+
+    return 0;
 }
